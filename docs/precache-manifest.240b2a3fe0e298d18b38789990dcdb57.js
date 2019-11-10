85d5e047758d485d47b288799084105bf58dcd67
Weihung <weihung@inno-orz.com>
2019-11-10 10:40:43 +0800

    Build new version

---
+++ docs/precache-manifest.240b2a3fe0e298d18b38789990dcdb57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9edc98d2f4c2e8a69c655cbf9222279",
+    "url": "/dice_demo/index.html"
+  },
+  {
+    "revision": "a3ba2c5f90854004d0e1",
+    "url": "/dice_demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "969ee44f99106e68fff2",
+    "url": "/dice_demo/static/js/2.9895bef2.chunk.js"
+  },
+  {
+    "revision": "a3ba2c5f90854004d0e1",
+    "url": "/dice_demo/static/js/main.2c291a69.chunk.js"
+  },
+  {
+    "revision": "c033385a4552c7b2a0a9",
+    "url": "/dice_demo/static/js/runtime-main.399989cb.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f8a09c2367078abd743d53a29c05451.js"
+  "/dice_demo/precache-manifest.240b2a3fe0e298d18b38789990dcdb57.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dice_demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

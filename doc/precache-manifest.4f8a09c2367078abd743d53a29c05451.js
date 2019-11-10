89c55d8766f9a3ed9e0428c1d3444e52bb257be8
Weihung <weihung@inno-orz.com>
2019-11-10 10:26:38 +0800

    First commit

---
+++ doc/precache-manifest.4f8a09c2367078abd743d53a29c05451.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291cb117d4822ca20befbd4e765ea557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7026a0c1f1d6bcbe4c6",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "969ee44f99106e68fff2",
+    "url": "/static/js/2.9895bef2.chunk.js"
+  },
+  {
+    "revision": "a7026a0c1f1d6bcbe4c6",
+    "url": "/static/js/main.756e8ad0.chunk.js"
+  },
+  {
+    "revision": "18f561a02aa7d7c32caf",
+    "url": "/static/js/runtime-main.a64d916c.js"
+  }
+]);

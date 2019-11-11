be9f9b2a5ec27882551d6cbb495bcd441ee3015b
Weihung <weihung@inno-orz.com>
2019-11-11 15:18:27 +0800

    Build new version

---
+++ docs/precache-manifest.51f4b5568640629cd261ef05e8e0fd01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712dc37f8b5ddbf7b2b715d7bcda0eb7",
+    "url": "https://weihung.github.io/dice_demo/index.html"
+  },
+  {
+    "revision": "2aa7b33449742a436d2d",
+    "url": "https://weihung.github.io/dice_demo/static/css/main.1b449076.chunk.css"
+  },
+  {
+    "revision": "f986033abaaa32983c0b",
+    "url": "https://weihung.github.io/dice_demo/static/js/2.cfe927b5.chunk.js"
+  },
+  {
+    "revision": "2aa7b33449742a436d2d",
+    "url": "https://weihung.github.io/dice_demo/static/js/main.ff12529d.chunk.js"
+  },
+  {
+    "revision": "174fc030eaa34fc87473",
+    "url": "https://weihung.github.io/dice_demo/static/js/runtime-main.7a6135f0.js"
+  }
+]);
